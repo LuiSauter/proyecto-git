5d0606f795c8a22a656931ceb0d8506c99c7935b
Luis Gabriel Janco <janco7249@gmail.com>
2024-01-10 22:56:35 -0400

    agregamos endpoint /login

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('node app')
 })
 
+app.use('/login', (req, res) => {
+  res.json('Loggeado')
+})
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto http://localhost:${port}`)
 })
